d5ac4ebabf5ac32c6dce77cb7f919d56f612f60f
Adriano Vinhas <vinhas.adriano@gmail.com>
2013-11-29 01:02:19 +0000

    Runnable version

--- tp2/src/dataset_train_valid_split.m
+++ tp2/src/dataset_train_valid_split.m
@@ -1,27 +1,40 @@
 %% "Importing" constants and structs
-constants_structs
+constants
 
 %% Load original dataset
-load P_dataset.dat;
+load('../dataset/train_and_validation_dataset.mat')
+
+number_of_patterns = size(X1,1);
+number_of_features = size(X1,2);
+TRAINING_SAMPLES = round(number_of_patterns*TRAINING_RATIO);
+VALIDATION_SAMPLES = number_of_patterns - TRAINING_SAMPLES;
 
 %% Split dataset into training and validation datasets
-P_train = zeros(DIGIT_SIZE.width * DIGIT_SIZE.height, N_DIGITS * TRAINING_SAMPLES_PER_DIGIT);
-P_valid = zeros(DIGIT_SIZE.width * DIGIT_SIZE.height, N_DIGITS * VALIDATION_SAMPLES_PER_DIGIT);
+normalizar=1;
+   
+%Normalizacao [0..1]
+if normalizar
+   for i=1:size(X1,1)
+       X1(i,:)=X1(i,:)/max(X1(i,:));
+   end
+end
 
-for i = 1 : N_DIGITS; 
-    % Shuffle i digit indexes on the dataset
-    P_digit_i_indexes = (i - 1) * SAMPLES_PER_DIGIT + randsample(SAMPLES_PER_DIGIT, SAMPLES_PER_DIGIT);
+%model = pca( X1 );
+%model.eigval
+[coefs,scores,latent] = princomp(X1);
+cumsum(latent)./sum(latent)
+X1 = scores(:,1:4);
 
-    % Pick the dataset's digit patterns on a shuffled manner, in order to obtain different training and validation datasets
-    P_digit_i = P_dataset(:, P_digit_i_indexes);
+% Shuffle i digit indexes on the dataset
+shuffled_patterns_indexes = randsample(number_of_patterns,number_of_patterns);
 
-    P_digit_i_train = P_digit_i(:, 1 : TRAINING_SAMPLES_PER_DIGIT);
-    P_digit_i_valid = P_digit_i(:, TRAINING_SAMPLES_PER_DIGIT + 1 : SAMPLES_PER_DIGIT);
+% Pick the dataset's digit patterns on a shuffled manner, in order to obtain different training and validation datasets
+shuffled_patterns = shuffled_patterns_indexes(shuffled_patterns_indexes,:);
 
-    % Avoid concatenation and use attribution
-    P_train(:, (i - 1) * TRAINING_SAMPLES_PER_DIGIT + 1 : i * TRAINING_SAMPLES_PER_DIGIT) = P_digit_i_train;
-    P_valid(:, (i - 1) * VALIDATION_SAMPLES_PER_DIGIT + 1 : i * VALIDATION_SAMPLES_PER_DIGIT) = P_digit_i_valid;
-end;
+X1_train = X1(1 : TRAINING_SAMPLES, :);
+T1_train = T1(1 : TRAINING_SAMPLES, :);
+X1_validation = X1(TRAINING_SAMPLES + 1 : number_of_patterns, :);
+T1_validation = T1(TRAINING_SAMPLES + 1 : number_of_patterns, :);
 
-save('P_train.dat', 'P_train', '-ascii');
-save('P_valid.dat', 'P_valid', '-ascii');
+save('../dataset/training_dataset', 'X1_train', 'T1_train', 'caracteristicas_names');
+save('../dataset/validation_dataset', 'X1_validation', 'T1_validation', 'caracteristicas_names');
