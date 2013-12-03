f9e385ffd4b52d38e4b1616ea022aa26fae92d82
José Ribeiro <jlbribeiro@gmail.com>
2013-12-03 19:34:38 +0000

    [dataset] temp files cleanup (and ignore); modified constants; more verbose main.m. [BUGFIX] scalestd.m now correctly normalizing input

--- tp2/src/constants.m
+++ tp2/src/constants.m
@@ -8,8 +8,8 @@ N_RUNS = 30;
 
 %% Training
 TRAINING_METHOD     = 'trainlm'; % Value to be fine-tuned ['trainlm', 'traingd']
-LEARNING_RATE       = 1e-5;      % Value to be fine-tuned [1e-3, 1e-4, 1e-5]    (N_HIDDEN_NEURONS = 5, N_FEATURES = 4)
-N_HIDDEN_NEURONS    = 8;         % Value to be fine-tuned [2, 5, 8, 10]         (best LEARNING_RATE, N_FEATURES = 4)
+LEARNING_RATE       = 1e-4;      % Value to be fine-tuned [1e-3, 1e-4, 1e-5]    (N_HIDDEN_NEURONS = 5, N_FEATURES = 4)
+N_HIDDEN_NEURONS    = 10;         % Value to be fine-tuned [2, 5, 8, 10]         (best LEARNING_RATE, N_FEATURES = 4)
 N_FEATURES          = 4;         % Value to be fine-tuned [2, 4, 6]             (best LEARNING_RATE, best N_HIDDEN_NEURONS)
 N_EPOCHS            = 100;       % Value to be fine-tuned [30, 50, 100]         (best LEARNING_RATE, best N_HIDDEN NEURONS, best N_FEATURES)
 INPUT_NORMALIZATION = true;      % Value to be fine-tuned [true, false]
--- tp2/src/scalestd.m
+++ tp2/src/scalestd.m
@@ -1,10 +1,10 @@
 function [ X_normalized ] = scalestd( X )
-	[n_features, ~] = size(X);
+	[~, n_features] = size(X);
 
     X_normalized = zeros(size(X));
 	for i = 1 : n_features
-	    mu = mean(X(i, :));
-	    sig = std(X(i, :));
-	    X_normalized(i, :) = (X(i, :) - mu) / sig; % scaling each feature to the unit range
+	    mu = mean(X(:, i));
+	    sig = std(X(:, i));
+	    X_normalized(:, i) = (X(:, i) - mu) / sig; % scaling each feature to the unit range
 	end
 end
